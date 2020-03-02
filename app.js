c74669b155367b9c18112cca0301c7437c7209b9
abdulmalikgh <abdul-malik.musah.stu@uenr.edu.gh>
2020-03-02 11:49:48 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const firstPasswordInput = document.querySelector('#first');
+const secondPasswordInput = document.querySelector('#second');
+const submit = document.querySelector('#submit');
+
+submit.onclick = function () {};
