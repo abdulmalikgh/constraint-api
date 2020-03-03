71dced3934a0e312e1ba61a4ed68ce060995c15d
abdulmalikgh <abdul-malik.musah.stu@uenr.edu.gh>
2020-03-02 21:13:47 -0800

    finish quiz

--- app.js
+++ app.js
@@ -2,5 +2,35 @@
 const firstPasswordInput = document.querySelector('#first');
 const secondPasswordInput = document.querySelector('#second');
 const submit = document.querySelector('#submit');
+const form = document.querySelector('form');
 
-submit.onclick = function () {};
+secondPasswordInput.addEventListener('blur', function(){
+    if(firstPasswordInput.value !== secondPasswordInput.value) {
+        firstPasswordInput.setCustomValidity("The Two email address must match.");
+    } else {
+        firstPasswordInput.setCustomValidity('')
+    } 
+    
+})
+
+form.addEventListener('submit', function(event){
+    if(form.checkValidity()  === false){
+        event.preventDefault();
+        return false;
+    }
+
+ event.preventDefault();
+ alert('Form is valid')
+})
+//usign pseudo classes to provide live feedback
+const inputs = document.querySelectorAll('input');
+const inputsLength = inputs.length;
+var addDirtyClass = function(e){
+    e.srcElement.classList.toggle('dirty', true);
+};
+for(let i=0; i<inputsLength; i++){
+    let input = inputs[i];
+    input.addEventListener('blur',addDirtyClass);
+    input.addEventListener('invalid',addDirtyClass);
+    input.addEventListener('valid', addDirtyClass)
+}
